5dec3d8b45b164755714c3e2029e10af2986554e
Roberta Teles <108440315+robertateles@users.noreply.github.com>
2022-09-28 00:55:21 -0300

    Update Rotas.js

--- src/Rotas.js
+++ src/Rotas.js
@@ -26,6 +26,7 @@ export default function Rotas() {
             <Route path='/edit/:id' element={<Edit/>}/>
             <Route path='/view/:id' element={<View/>}/>  
         </Routes>
+        <Home/>
         <Footer/>
     </Router>
   )
