2a49e0a545510adbfb086936e811006347400526
Roberta Teles <108440315+robertateles@users.noreply.github.com>
2022-09-28 00:22:17 -0300

    adicionando o correto

--- src/Rotas.js
+++ src/Rotas.js
@@ -7,6 +7,9 @@ import Promocao from './pages/Promocao';
 import Contato from './pages/Contato';
 import Login from './pages/Login';
 import Footer from './components/Footer';
+import Edit from './pages/Login/edit';
+import Tabela from './pages/Login/tabela';
+import View from './pages/Login/view';
 
 
 export default function Rotas() {
@@ -19,7 +22,9 @@ export default function Rotas() {
             <Route path='/promocao' element={<Promocao/>}/>
             <Route path='/contato' element={<Contato/>}/>
             <Route path='/login' element={<Login/>}/>
-
+            <Route path='/tabela' element={<Tabela/>}/>
+            <Route path='/edit/:id' element={<Edit/>}/>
+            <Route path='/view/:id' element={<View/>}/>  
         </Routes>
         <Footer/>
     </Router>
